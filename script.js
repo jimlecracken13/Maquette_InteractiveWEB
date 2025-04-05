7ef982276078c5e1f97b39effb595672d3071d49
jimlecraque <adiallo7485@gmail.com>
2025-04-05 15:21:25 +0000

    responsivité débuté

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function voiCV()
+function cv()
 {
-    
+    window.location.href='cv.html'
+}
+function changeCouleur(id)
+{
+    document.getElementById(id).style.backgroundColor='#73b8d8';
+    console.log("click")
+}
+function telechargeCV(id)
+{
+    var element = document.getElementById(id);
+    var opt = {
+    margin:       1,//pour les marges du pdf
+    filename:     'cv.pdf',//pour le nom du fichier
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' },
+    pagebreak:    {mode: ['avoid-all','css','legacy']}
+    };
+
+    // New Promise-based usage:
+    html2pdf().set(opt).from(element).save();
+}
+function retour()
+{
+    window.location.href='index.html'
 }
