860a7926b058f21bfc70f2067d859bb1a64448a7
jimlecracken14 <jimlecracken13@gmail.com>
2025-04-07 17:22:59 +0000

    mise à jour du cv

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function telechargeCV(id)
     filename:     'cv.pdf',//pour le nom du fichier
     image:        { type: 'jpeg', quality: 0.98 },
     html2canvas: { scale: 2 },
-    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' },
+    jsPDF:        { unit: '', format: 'a4', orientation: 'portrait' },
     pagebreak:    {mode: ['avoid-all','css','legacy']}
     };
 
